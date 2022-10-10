2f1d20752a7bbc5af9cc28bdd69ece2d9d26ba07
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2022-10-10 14:28:37 +0200

    Fix import

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description="A Python interface to BlueNaaS-Subcellular. It allows users to import BNGL models, create and run simulations with STEPs and BioNetGen.",
     author="Blue Brain Project, EPFL",
     author_email="",
-    packages=["bluepysc"],  # same as name
+    packages=["BluePySubcellular"],  # same as name
     install_requires=["requests"],  # external packages as dependencies
 )
